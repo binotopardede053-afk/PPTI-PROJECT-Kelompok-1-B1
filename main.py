8e5e7ea38741f966264e17985929e34e02e759ea
BINOTO PARDEDE <binotopardede053@gmail.com>
2025-11-29 20:23:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import pygame
+from menu import main_menu
+from game import game_loop
+
+pygame.init()
+screen = pygame.display.set_mode((450, 700))
+pygame.display.set_caption("Flappy Bird")
+
+while True:
+    action = main_menu(screen)
+    if action == "start":
+        result = game_loop(screen)
+        if result == "menu":
+            continue
+    else:
+        break
 
